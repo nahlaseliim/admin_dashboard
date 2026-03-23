147be2138964072a62ebe2635e6b4b25527bcddf
powar <nahlaaselim@gmail.com>
2026-03-23 08:49:03 +0200

    fix dashboard scroll and layout height issue

--- lib/features/dashboard/ui/dashboard_screen.dart
+++ lib/features/dashboard/ui/dashboard_screen.dart
@@ -7,9 +7,7 @@ class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dashboard'),
-      ),
+
       body:
       Row(
         children: [
@@ -73,58 +71,63 @@ class DashboardScreen extends StatelessWidget {
     color: Colors.grey.shade100,
     padding: const EdgeInsets.all(16),
     child: SingleChildScrollView(
-      child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          minHeight: MediaQuery.of(context).size.height,
+        ),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
 
-      children: [
-      const Text(
-      "Dashboard Overview",
-      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-      ),
-
-      const SizedBox(height: 8),
+        children: [
         const Text(
-          "Here’s what’s happening today",
-          style: TextStyle(color: Colors.grey),
+        "Dashboard Overview",
+        style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
         ),
 
-        const SizedBox(height: 25),
-        Row(
-          children: const [
-            DashboardCard(
-              title: "Users",
-              value: "1200",
-              icon: Icons.people,
-              color: Colors.blue,
-            ),
-            DashboardCard(
-              title: "Orders",
-              value: "320",
-              icon: Icons.shopping_cart,
-              color: Colors.orange,
-            ),
-          ],
-        ),
+        const SizedBox(height: 8),
+          const Text(
+            "Here’s what’s happening today",
+            style: TextStyle(color: Colors.grey),
+          ),
 
-        const SizedBox(height: 20),
+          const SizedBox(height: 25),
+          Row(
+            children: const [
+              DashboardCard(
+                title: "Users",
+                value: "1200",
+                icon: Icons.people,
+                color: Colors.blue,
+              ),
+              DashboardCard(
+                title: "Orders",
+                value: "320",
+                icon: Icons.shopping_cart,
+                color: Colors.orange,
+              ),
+            ],
+          ),
 
-        Row(
-          children: const [
-            DashboardCard(
-              title: "Revenue",
-              value: "\$5,430",
-              icon: Icons.attach_money,
-              color: Colors.green,
-            ),
-            DashboardCard(
-              title: "Reports",
-              value: "12",
-              icon: Icons.bar_chart,
-              color: Colors.purple,
-            ),
-          ],
+          const SizedBox(height: 20),
+
+          Row(
+            children: const [
+              DashboardCard(
+                title: "Revenue",
+                value: "\$5,430",
+                icon: Icons.attach_money,
+                color: Colors.green,
+              ),
+              DashboardCard(
+                title: "Reports",
+                value: "12",
+                icon: Icons.bar_chart,
+                color: Colors.purple,
+              ),
+            ],
+          ),
+        ]
         ),
-      ]
       ),
     )
     )

18dbc22ad9588e5728a26de95961cbceb1375fdd
powar <nahlaaselim@gmail.com>
2026-03-27 05:13:35 +0200

    feat: add api constants folder & refactor: move api urls to constants

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const String baseUrl = 'https://jsonplaceholder.typicode.com';
+  static const String userEndpoint = '/users';
+  static const String orderEndpoint = '/posts';
+  static const String reportEndpoint = '/comments';
+
+}
